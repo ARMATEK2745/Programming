b534b9efcd99464d496b202d11773bf74e3b2e10
Арман Тасмаганбетов <armatek2745@gmail.com>
2026-04-10 19:18:49 +0700

    LAB 4. Создан класс Ring.

---
+++ Programming/Model/Ring.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    internal class Ring
+    {
+        private Point2D centerCircle;
+        private double radiusOuterCircle;
+        private double radiusInnerCircle;
+
+        public Point2D CenterCircle
+        {
+            get { return centerCircle; }
+            set { centerCircle = value; }
+        }
+
+        public double RadiusOuterCircle
+        {
+            get { return radiusOuterCircle; }
+            set
+            {
+                radiusOuterCircle = Validator.AssertOnPositiveValue(value, nameof(radiusOuterCircle));
+            }
+        }
+
+        public double RadiusInnerCircle
+        {
+            get { return radiusInnerCircle; }
+            set
+            {
+                radiusInnerCircle = Validator.AssertValueInRange(value, 0, radiusOuterCircle, nameof(radiusInnerCircle));
+            }
+        }
+
+        public double Area
+        {
+            get { return Math.PI * Math.Pow(radiusOuterCircle, 2) - Math.PI * Math.Pow(radiusInnerCircle, 2); }
+        }
+
+        public Ring(Point2D centerCircle, double radiusOuterCircle, double radiusInnerCircle)
+        {
+            CenterCircle = centerCircle;
+            RadiusOuterCircle = radiusOuterCircle;
+            RadiusInnerCircle = radiusInnerCircle;
+        }
+
+        public Ring() { }
+    }
+}
